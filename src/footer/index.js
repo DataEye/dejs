4aaf713430d118d8c7288e0cbb65cb9f1a6add4c
bob <bob@bob.local>
2017-03-13 16:19:33 +0800

    更新footer

--- src/footer/index.js
+++ src/footer/index.js
@@ -18,49 +18,52 @@ export default React.createClass({
           <div className="footer-info">
             <dl>
               <dt>产品与服务</dt>
-              <dd><a href={`${host}/adve.html`}>广告效果监测</a></dd>
-              <dd><a href={`${host}/channel.html`}>移动应用统计分析</a></dd>
-              <dd><a href={`${host}/product.html`}>移动游戏分析</a></dd>
-              <dd><a href={`${host}/h5.html`}>HTML5分析</a></dd>
-              <dd><a href={`${host}/dmp.html`}>泛娱乐DMP</a></dd>
+              <dd><a target="_blank" href={`${host}/dmp.html`}>DMP</a></dd>
+              <dd><a target="_blank" href={`${host}/dms.html`}>数据管家</a></dd>
+              <dd><a target="_blank" href={`${host}/adve_v2.html`}>统计分析平台</a></dd>
             </dl>
             <dl>
               <dt>数据报告</dt>
-              <dd><a href={`${host}/report.html#game`}>游戏分析报告</a></dd>
-              <dd><a href={`${host}/report.html#ent`}>泛娱乐行业报告</a></dd>
-              <dd><a href={`${host}/report.html#project`}>专题监测报告</a></dd>
+              <dd><a target="_blank" href={`${host}/report?type=game`}>游戏分析报告</a></dd>
+              <dd><a target="_blank" href={`${host}/report?type=automobile`}>汽车行业报告</a></dd>
+              <dd><a target="_blank" href={`${host}/report?type=project`}>专题监测报告</a></dd>
             </dl>
             <dl>
               <dt>合作展示</dt>
-              <dd><a href={`${host}/partners.html#games_module`}>案例合作游戏</a></dd>
-              <dd><a href={`${host}/partners.html#companies_module`}>客户寄语</a></dd>
-              <dd><a href={`${host}/partners.html#users_module`}>合作客户</a></dd>
+              <dd><a target="_blank" href={`${host}/partners.html#games_module`}>案例合作游戏</a></dd>
+              <dd><a target="_blank" href={`${host}/partners.html#users_module`}>合作客户</a></dd>
             </dl>
             <dl>
               <dt>关于我们</dt>
-              <dd><a href={`${host}/about.html`}>公司简介</a></dd>
-              <dd><a href={`${host}/partners.html`}>企业文化</a></dd>
-              <dd><a href={`${host}/job.html`}>招聘信息</a></dd>
+              <dd><a target="_blank" href={`${host}/about.html`}>公司简介</a></dd>
             </dl>
           </div>
           <div className="about">
             <span className="weixin"></span>
             <ul>
               <li><i className="fa fa-phone"></i>400-648-2833</li>
-              <li><i className="fa fa-qq"></i>400-648-2833</li>
-              <li><i className="fa fa-envelope"></i>customer@dataeye.com</li>
+              <li>
+                <i className="fa fa-qq"></i>
+                <a href="http://wpa.qq.com/msgrd?v=3&uin=3173109221&site=qq&menu=yes" target="_blank">3173109221 </a>、
+                <a href="http://wpa.qq.com/msgrd?v=3&uin=2030196706&site=qq&menu=yes" target="_blank">2030196706 </a>
+              </li>
+              <li><i className="fa fa-envelope"></i>support@dataeye.com</li>
             </ul>
           </div>
         </div>
-        <div className="clear"></div>
+        <div className="inner partner-inner">
+          <p>
+            <span>旗下品牌:</span><a href="https://www.splus.cn" target="_blank"><img src={`${host}/assets/img/splus_logo.png`} className="splus-logo" /></a>
+          </p>
+        </div>
         <div className="copyright">
+          <a href="http://szcert.ebs.org.cn/cd987215-bdcd-4a5e-a936-cc59970970cd" target="_blank">
+            <img src="http://szcert.ebs.org.cn/Images/govIcon.gif" title="深圳市市场监督管理局企业主体身份公示" alt="深圳市市场监督管理局企业主体身份公示" width="20" height="28" border="0" style={{borderWidth:'0px', border: 'none'}} />
+          </a>
           <span className="security">
             Copyright &copy; {year} DataEye 深圳市慧动创想科技有限公司
             <a target="_blank" href="http://www.miitbeian.gov.cn/">粤ICP备13074195-1号</a>
           </span>
-          <a target="_blank" href="http://szcert.ebs.org.cn/6c5c8adf-2bec-43fc-b927-cc3d3a70f866">
-            <img width="112" height="40" src="https://www.dataeye.com/assets/img/newGovIcon.gif" />
-          </a>
         </div>
       </div>
     )
